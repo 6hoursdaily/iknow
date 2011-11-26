369c14ad50257639a09a3beee86691eb51b3827c
jerry <jerrypascua@gmail.com>
2011-11-27 01:37:17 +0800

    "initial commit"

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Iknow::Application.config.secret_token = 'd76eb9ff67caf48420850b0c53300fd0eda39212ab0d7b362bc4026670ef820d4c4fd71566ea517b9fa2239c6139ec5ebc3ea25ec365ea53f401f75a20f0f327'
